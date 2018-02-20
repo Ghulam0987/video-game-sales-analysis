8dd45b6abe042e75033a22328e8f70c376a6e0a3
Guilherme Vazquez <guilhermevazquez@hotmail.com>
2018-02-20 19:21:35 -0300

    updating global sales by genre

--- video game analysis.R
+++ video game analysis.R
@@ -7,15 +7,18 @@ games <- read.csv("vgsales.csv") # source: https://www.kaggle.com/gregorut/video
 #pc1 <- ggplot(games, aes(x = Genre, fill = Genre))
 #pc1 + geom_bar()
 
-# Global Sales by Genre
+### Global Sales by Genre
 global_sales_by_genre <- aggregate(Global_Sales~Genre, games, sum)
-global_sales_by_genre <- global_sales_by_genre[order(global_sales_by_genre$Global_Sales),]
-ggplot(global_sales_by_genre, aes(x=Genre, y=Global_Sales)) + 
+# plot
+ggplot(global_sales_by_genre, aes(x= reorder(Genre, -Global_Sales), y=Global_Sales)) + 
   geom_bar(stat="identity", width=.5, aes(fill = Genre)) + 
   geom_text(aes(label=Global_Sales), vjust=-0.3, color="black", size=3.5) +
-  labs(title="Video Game Global Sales (in millions) by Genre", 
+  labs(title="Video Game Global Sales (millions of copies) by Genre", 
        subtitle="Genre Vs Global Sales", 
-       caption="source: vgchartz.com") + 
+       caption="source: vgchartz.com",
+       x="Genre",
+       y="Global Sales (millions of copies)") + 
   theme(axis.text.x = element_text(angle=65, vjust=0.6))
-
+# save
+ggsave("global sales by genre.jpg", last_plot())
 
